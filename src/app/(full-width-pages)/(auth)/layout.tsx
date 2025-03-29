d5950ec524e5e95b59a96b40b3a447102cc80cd3
Rotacho <rotachocool@gmail.com>
2025-03-29 12:59:01 -0400

    UI Login

--- src/app/(full-width-pages)/(auth)/layout.tsx
+++ src/app/(full-width-pages)/(auth)/layout.tsx
@@ -13,33 +13,21 @@ export default function AuthLayout({
 }) {
   return (
     <div className="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
-      <ThemeProvider>
-        <div className="relative flex lg:flex-row w-full h-screen justify-center flex-col  dark:bg-gray-900 sm:p-0">
+      <div className="absolute inset-0 -z-10">
+        <Image
+          src="/background1.png"   /* Ajusta la ruta según tu estructura de archivos */
+          alt="Fondo"
+          fill        /* Ocupa todo el contenedor */
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+        <div className="relative flex items-center justify-center min-h-screen ">
           {children}
-          <div className="lg:w-1/2 w-full h-full bg-brand-950 dark:bg-white/5 lg:grid items-center hidden">
-            <div className="relative items-center justify-center  flex z-1">
-              {/* <!-- ===== Common Grid Shape Start ===== --> */}
-              <GridShape />
-              <div className="flex flex-col items-center max-w-xs">
-                <Link href="/" className="block mb-4">
-                  <Image
-                    width={231}
-                    height={48}
-                    src="./images/logo/auth-logo.svg"
-                    alt="Logo"
-                  />
-                </Link>
-                <p className="text-center text-gray-400 dark:text-white/60">
-                  Free and Open-Source Tailwind CSS Admin Dashboard Template
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="fixed bottom-6 right-6 z-50 hidden sm:block">
+
+          {/* <div className="fixed bottom-6 right-6 z-50 hidden sm:block">
             <ThemeTogglerTwo />
-          </div>
+          </div> Cambiar el tema*/}
         </div>
-      </ThemeProvider>
     </div>
   );
 }
